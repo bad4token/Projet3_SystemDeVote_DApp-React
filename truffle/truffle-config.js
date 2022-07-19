6c3880375462b1a0b7dc1ba4dc94ea2f5d3d5e1e
elgrego <bad4token@gmail.com>
2022-07-19 15:27:07 +0200

    gh-page maj

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -16,17 +16,7 @@ module.exports = {
      ropsten:{
       provider : function() { return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`}, providerOrUrl:`https://ropsten.infura.io/v3/${process.env.INFURA_ID}`})},
       network_id:3,
-     },
-/*
-    ropsten: {
-      provider: () => new HDWalletProvider('dove balance bargain cave recall verb pause innocent unusual boost vacuum dance', 'https://ropsten.infura.io/v3/75eefb9868c74cccb605c888eafaa629'),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },
-*/
+     }
 
   },
 
